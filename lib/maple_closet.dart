043089f1b5cab18f789d7d120b55a22eb0dceea2
hammsik <asbbrp@gmail.com>
2023-10-10 16:41:01 +0900

    appbar 및 endDrawer 구현

--- lib/maple_closet.dart
+++ lib/maple_closet.dart
@@ -1,29 +1,74 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:maple_closet/layout_character_board.dart';
 import 'package:maple_closet/layout_coordinating_tool.dart';
 
-class MapleCloset extends StatelessWidget {
+class MapleCloset extends StatefulWidget {
   const MapleCloset({super.key});
 
   @override
+  State<StatefulWidget> createState() => _MapleCloset();
+}
+
+class _MapleCloset extends State<MapleCloset> {
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+
+  void _openEndDrawer() {
+    _scaffoldKey.currentState!.openEndDrawer();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(
+      SystemUiOverlayStyle(statusBarColor: Colors.transparent),
+    );
+
     return MaterialApp(
       home: Scaffold(
+        key: _scaffoldKey,
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text(
+            "MAPLE CLOSET",
+            style: TextStyle(color: Colors.black),
+          ),
+          leading: IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.info_outlined,
+              color: Colors.black,
+              size: 30,
+            ),
+            alignment: Alignment.center,
+          ),
+          actions: [
+            IconButton(
+              onPressed: _openEndDrawer,
+              icon: Icon(Icons.account_box),
+              color: Colors.black,
+            ),
+          ],
+          elevation: 5,
+          backgroundColor: const Color.fromARGB(255, 250, 250, 250),
+        ),
+        endDrawer: Drawer(),
         body: Container(
           width: MediaQuery.of(context).size.width,
           height: MediaQuery.of(context).size.height,
-          padding: EdgeInsets.all(22),
+          decoration:
+              const BoxDecoration(color: Color.fromARGB(255, 250, 250, 250)),
+          padding: const EdgeInsets.all(22),
           child: Column(
             children: [
               CharacterBoard(),
-              SizedBox(height: 14),
+              const SizedBox(height: 14),
               Container(
                 height: 1,
                 width: double.infinity,
                 margin: EdgeInsets.only(left: 7, right: 7),
                 color: Color.fromARGB(255, 108, 108, 108),
               ),
-              SizedBox(height: 14),
+              const SizedBox(height: 14),
               Expanded(child: CoordinatingTools()),
             ],
           ),
