6ed74465ec59f6f11b618fe2b0899d4434841d45
hammsik <asbbrp@gmail.com>
2023-10-11 21:02:53 +0900

    version2: custom app bar로 전환

---
+++ lib/layout_custom_app_bar.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class MyCustomAppBar extends StatefulWidget {
+  final Function() clickEvent;
+
+  MyCustomAppBar({super.key, required this.clickEvent});
+
+  @override
+  State<StatefulWidget> createState() {
+    return _MyCustomAppBar();
+  }
+}
+
+class _MyCustomAppBar extends State<MyCustomAppBar> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        GestureDetector(
+          child: Icon(
+            Icons.info_outlined,
+            color: Colors.black,
+          ),
+        ),
+        Text(
+          "MAPLE CLOSET",
+          style: GoogleFonts.nanumMyeongjo(
+            color: Colors.black,
+            fontSize: 20,
+          ),
+        ),
+        GestureDetector(
+          onTap: widget.clickEvent,
+          child: Icon(
+            Icons.account_box,
+            color: Colors.black,
+          ),
+        ),
+      ],
+    );
+  }
+}
--- lib/maple_closet.dart
+++ lib/maple_closet.dart
@@ -3,6 +3,7 @@ import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:maple_closet/layout_character_board.dart';
 import 'package:maple_closet/layout_coordinating_tool.dart';
+import 'package:maple_closet/layout_custom_app_bar.dart';
 
 class MapleCloset extends StatefulWidget {
   const MapleCloset({super.key});
@@ -21,65 +22,37 @@ class _MapleCloset extends State<MapleCloset> {
   @override
   Widget build(BuildContext context) {
     SystemChrome.setSystemUIOverlayStyle(
-      const SystemUiOverlayStyle(statusBarColor: Colors.transparent),
+      const SystemUiOverlayStyle(statusBarIconBrightness: Brightness.dark),
     );
 
     return MaterialApp(
       home: Scaffold(
         key: _scaffoldKey,
-        appBar: AppBar(
-          centerTitle: true,
-          leading: IconButton(
-            padding: EdgeInsets.only(left: 24),
-            onPressed: () {},
-            icon: Icon(
-              Icons.info_outlined,
-              color: Colors.black,
-            ),
-            iconSize: 24,
-            alignment: Alignment.center,
-          ),
-          title: Text(
-            "MAPLE CLOSET",
-            style: GoogleFonts.nanumMyeongjo(
-              color: Colors.black,
-              fontSize: 18,
-            ),
-            textAlign: TextAlign.end,
-          ),
-          actions: [
-            IconButton(
-              padding: EdgeInsets.only(right: 8),
-              onPressed: _openEndDrawer,
-              icon: Icon(Icons.account_box),
-              color: Colors.black,
-              iconSize: 24,
-            ),
-          ],
-          elevation: 0,
-          backgroundColor: const Color.fromARGB(255, 250, 250, 250),
-        ),
         endDrawer: Drawer(),
-        body: Container(
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height,
-          decoration:
-              const BoxDecoration(color: Color.fromARGB(255, 250, 250, 250)),
-          padding:
-              const EdgeInsets.only(left: 20, top: 10, right: 20, bottom: 20),
-          child: Column(
-            children: [
-              CharacterBoard(),
-              const SizedBox(height: 14),
-              Container(
-                height: 1,
-                width: double.infinity,
-                margin: EdgeInsets.only(left: 7, right: 7),
-                color: Color.fromARGB(255, 108, 108, 108),
-              ),
-              const SizedBox(height: 14),
-              CoordinatingTools(),
-            ],
+        body: SafeArea(
+          child: Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            decoration:
+                const BoxDecoration(color: Color.fromARGB(255, 250, 250, 250)),
+            padding: const EdgeInsets.only(left: 20, right: 20, bottom: 20),
+            child: Column(
+              children: [
+                const SizedBox(height: 14),
+                MyCustomAppBar(clickEvent: _openEndDrawer),
+                const SizedBox(height: 14),
+                CharacterBoard(),
+                const SizedBox(height: 14),
+                Container(
+                  height: 1,
+                  width: double.infinity,
+                  margin: EdgeInsets.only(left: 7, right: 7),
+                  color: Color.fromARGB(255, 108, 108, 108),
+                ),
+                const SizedBox(height: 14),
+                CoordinatingTools(),
+              ],
+            ),
           ),
         ),
       ),
