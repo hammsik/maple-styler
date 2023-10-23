82042b8b177433dbf199174646b4568d65fe4287
hammsik <asbbrp@gmail.com>
2023-10-24 04:15:01 +0900

    Fix: 메이플 io api test 해결
    image.network는 url로부터 이미지를 캐싱하여 바로 출력해주는 것. gif도 받아준다. 기본적으로 비동기로 작동한다.

--- lib/test.dart
+++ lib/test.dart
@@ -6,7 +6,7 @@ const API_KEY =
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJYLUFwcC1SYXRlLUxpbWl0IjoiNTAwOjEwIiwiYWNjb3VudF9pZCI6IjEyOTIxOTU4NzAiLCJhdXRoX2lkIjoiMiIsImV4cCI6MTcxMzYxMzE1NiwiaWF0IjoxNjk4MDYxMTU2LCJuYmYiOjE2OTgwNjExNTYsInNlcnZpY2VfaWQiOiI0MzAwMTEzOTciLCJ0b2tlbl90eXBlIjoiQWNjZXNzVG9rZW4ifQ.7pcpCzYJHwIq5QTOvmkktcmsISFUZTXPxPnL6pQvlQ0';
 // ignore: constant_identifier_names
 const MAPLE_CUBE_RESULT_BASE_URL =
-    'https://public.api.nexon.com/openapi/maplestory/v1/cube-use-results';
+    'https://maplestory.io/api/character/{"itemId":2000,"alpha":1,"region":"KMST","version":"1157"},{"itemId":12000,"alpha":1,"region":"KMST","version":"1157"},{"itemId":1060000,"region":"KMST","version":"1157"}/stand1/animated';
 
 class CubeModel {
   final cubeType;
@@ -19,19 +19,23 @@ class APIPractice {
   static int count = 10;
   static String date = '2023-10-17';
 
-  static Future<List<CubeModel>> getSomething() async {
-    Dio().options.headers['authorization'] = 'Bearer $API_KEY';
-    final resp = await Dio().get(MAPLE_CUBE_RESULT_BASE_URL, queryParameters: {
-      'count': APIPractice.count,
-      'date': APIPractice.date,
-    });
-    final listWithData = resp.data['cube_histories'];
+  // static Future<List<CubeModel>> getSomething() async {
+  //   final resp = await Dio().get(MAPLE_CUBE_RESULT_BASE_URL,
+  //       queryParameters: {
+  //         'count': APIPractice.count,
+  //         'date': APIPractice.date,
+  //       },
+  //       options: Options(headers: {'authorization': 'API_KEY'}));
+  //   final listWithData = resp.data['cube_histories'];
 
-    return listWithData
-        .map((item) => CubeModel(
-            cubeType: item['cube_type'],
-            potentialblabla: item['additional_potential_option_grade']))
-        .toList;
+  //   return listWithData
+  //       .map((item) => CubeModel(
+  //           cubeType: item['cube_type'],
+  //           potentialblabla: item['additional_potential_option_grade']))
+  //       .toList;
+  // }
+  static Future<dynamic> getSomething() async {
+    return await Dio().get(MAPLE_CUBE_RESULT_BASE_URL);
   }
 }
 
@@ -47,35 +51,16 @@ class MyTest extends StatelessWidget {
       decoration: BoxDecoration(color: Color(0xff222222)),
       child: Container(
         alignment: Alignment.center,
-        width: MediaQuery.of(context).size.width * 0.7,
-        height: MediaQuery.of(context).size.height * 0.7,
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
         decoration: BoxDecoration(color: Color(0xff333333)),
-        child: FutureBuilder(
-          future: APIPractice.getSomething(),
-          builder: (context, snapshot) {
-            if (snapshot.hasError) {
-              return const Text(
-                'error',
-                style: TextStyle(color: Color(0xffffffff)),
-              );
-            }
-
-            if (snapshot.hasData) {
-              return CircularProgressIndicator();
-            }
-
-            return Column(
-              children: [
-                ...snapshot.data!.map(
-                  (item) => Text(
-                    item.cubeType + item.potentialblabla,
-                    style: TextStyle(color: Color(0xffffffff)),
-                  ),
-                ),
-              ],
-            );
-          },
-        ),
+        child: Container(
+          decoration: BoxDecoration(color: Color.fromARGB(255, 141, 125, 125)),
+          child: Image.network(
+            'https://maplestory.io/api/character/{"itemId":2000,"alpha":1,"region":"KMST","version":"1157"},{"itemId":12000,"alpha":1,"region":"KMST","version":"1157"},{"itemId":1060000,"region":"KMST","version":"1157"}/stand1/animated',
+            height: 300,
+          ),
+        ), // 이미지를 가져오는 비동기 작업
       ),
     );
   }
