1da2f4fbb9c2700c495107cb8f0e379f5c768694
hammsik <asbbrp@gmail.com>
2023-10-25 02:04:06 +0900

    fix: 리스트 불러오기 성공!

--- lib/test.dart
+++ lib/test.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:dio/dio.dart';
+import 'dart:convert';
 
 // ignore: constant_identifier_names
 const API_KEY =
@@ -15,8 +16,8 @@ class CubeModel {
 }
 
 class APIPractice {
-  static Future<List<dynamic>> getSomething() async {
-    final resp = await Dio().get(
+  static Future<List<List<dynamic>>> getSomething() async {
+    final Response response = await Dio().get(
       MAPLE_CUBE_RESULT_BASE_URL,
       queryParameters: {
         'overallCategoryFilter': 'Equip',
@@ -24,18 +25,25 @@ class APIPractice {
         'subCategoryFilter': 'Hat',
       },
     );
-    print('ㅁㄴㅇㄹ');
-    print(resp.data.runtimeType);
-    final listWithData = resp.data
-        .map((data) => {
-              "id": data["id"],
-              "name": data["name"],
-            })
-        .toList();
+    // print('ㅁㄴㅇㄹ');
+    // print(response.data);
+    // print(response.data[0].runtimeType);
 
-    print('ㄴㅇㄹ');
-    print(listWithData.runtimeType);
-    return listWithData;
+    // for (Map<String, dynamic> m in response.data) {
+    //   print('${m['id']} / ${m['name']}');
+    // }
+    List<List<dynamic>> myList = List<List<String>>.from(response.data
+        .map((data) => [data['id'].toString(), data['name'].toString()]));
+    myList.shuffle();
+    // print(dataList);
+    // final List<Map<String, String>> listWithData2 =
+    //     List<Map<String, String>>.from(listWithData);
+    // print('과연 listWithData의 타입은?');
+    // print(listWithData2.runtimeType);
+    // print(ilist[0]['asd']);
+    // print('ㄴㅇㄹ');
+    // print(listWithData.runtimeType);
+    return myList;
   }
 }
 
@@ -56,17 +64,18 @@ class MyTest extends StatelessWidget {
         decoration: BoxDecoration(color: Color.fromARGB(255, 116, 116, 116)),
         child: Container(
           decoration: BoxDecoration(color: Color.fromARGB(255, 141, 125, 125)),
-          child: FutureBuilder<List<dynamic>>(
+          child: FutureBuilder<List<List<dynamic>>>(
             future: APIPractice.getSomething(),
             builder: (context, snapshot) {
-              print(snapshot.data);
               if (snapshot.hasError) {
                 return Text(snapshot.error.toString());
               } else {
                 return ListView(
                   physics: BouncingScrollPhysics(),
-                  children:
-                      snapshot.data!.map((item) => Text(item['name'])).toList(),
+                  children: [
+                    ...snapshot.data!
+                        .map((item) => Text(item[0] + ' ' + item[1]))
+                  ],
                 );
               }
             },
