3e6d5d335c0b5b037a7cc71afccf27b8f9036675
hammsik <asbbrp@gmail.com>
2023-10-26 04:27:56 +0900

    Feat: Mycharacter class 파일 추가

--- lib/models/skeleton_myCharacter.dart
+++ lib/models/skeleton_myCharacter.dart
@@ -1,30 +1,95 @@
 class MyCharacter {
   // 뷰티
-  final String skinColor;
-  final String hair;
-  final String face;
+  final String? skinColor;
+  final String? hair;
+  final String? face;
   // 장비
-  final String weapon;
-  final String overAll;
-  final String hat;
-  final String top;
-  final String bottom;
-  final String cape;
-  final String glove;
-  final String shoes;
-  final String shield;
+  final String? weapon;
+  final String? overall;
+  final String? hat;
+  final String? top;
+  final String? bottom;
+  final String? cape;
+  final String? glove;
+  final String? shoes;
+  final String? shield;
 
   MyCharacter(
-      {required this.skinColor,
-      required this.hair,
-      required this.face,
-      required this.weapon,
-      required this.overAll,
-      required this.hat,
-      required this.top,
-      required this.bottom,
-      required this.cape,
-      required this.glove,
-      required this.shoes,
-      required this.shield});
+      {this.skinColor,
+      this.hair,
+      this.face,
+      this.weapon,
+      this.overall,
+      this.hat,
+      this.top,
+      this.bottom,
+      this.cape,
+      this.glove,
+      this.shoes,
+      this.shield});
+
+  String addVersionAndRegion(String item) {
+    return '{"itemId":${item}, "version":"1157","region":"KMST","animationName":"default"},';
+  }
+
+  String makeItemsURL() {
+    String items = "";
+    if (skinColor != null) {
+      items += addVersionAndRegion(skinColor!.substring(1));
+      items += addVersionAndRegion(skinColor!);
+    } else {
+      items += addVersionAndRegion('2016');
+      items += addVersionAndRegion('12016');
+    }
+    if (hair != null) {
+      items += addVersionAndRegion(hair!);
+    } else {
+      items += addVersionAndRegion('68090');
+    }
+    if (face != null) {
+      items += addVersionAndRegion(face!);
+    } else {
+      items += addVersionAndRegion('50137');
+    }
+    if (top != null) {
+      items += addVersionAndRegion(top!);
+    } else if (overall == null) {
+      items += addVersionAndRegion('1040036');
+    }
+    if (bottom != null) {
+      items += addVersionAndRegion(bottom!);
+    } else if (overall == null) {
+      items += addVersionAndRegion('1060026');
+    }
+    if (weapon != null) {
+      items += addVersionAndRegion(weapon!);
+    }
+    if (overall != null) {
+      items += addVersionAndRegion(overall!);
+    }
+    if (hat != null) {
+      items += addVersionAndRegion(hat!);
+    }
+    if (cape != null) {
+      items += addVersionAndRegion(cape!);
+    }
+    if (glove != null) {
+      items += addVersionAndRegion(glove!);
+    }
+    if (shoes != null) {
+      items += addVersionAndRegion(shoes!);
+    }
+    if (shield != null) {
+      items += addVersionAndRegion(shield!);
+    }
+
+    return items;
+  }
+
+  String getMyCharacterURL() {
+    String middle = makeItemsURL();
+    return 'https://maplestory.io/api/Character/$middle/stand1/0/?renderMode=2';
+  }
 }
+  // final String head = 'https://maplestory.io/api/Character/';
+  // const String tail = '/stand1/0/?renderMode=2';
