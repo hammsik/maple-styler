7bc033d65e64823af77eb3c6f9f82ec7aa19b717
hammsik <asbbrp@gmail.com>
2023-10-27 02:02:31 +0900

    Refactor: 생성자 제거 및 작은 버그들 수정

--- lib/models/skeleton_myCharacter.dart
+++ lib/models/skeleton_myCharacter.dart
@@ -1,12 +1,12 @@
 class MyCharacter {
   // 뷰티
-  String hair;
-  String face;
-  String head;
-  String body;
+  String hair = '68090';
+  String face = '50137';
+  String head = '12016';
+  String body = '2016';
   // 장비
-  String top;
-  String bottom;
+  String top = '1040036';
+  String bottom = '1060026';
   String? hat;
   String? overall;
   String? cash;
@@ -14,22 +14,29 @@ class MyCharacter {
   String? glove;
   String? shoes;
   String? shield;
+  //악세
+  String? faceAccessory;
+  String? eyeDecoration;
+  String? earrings;
 
-  MyCharacter({
-    this.hair = '68090',
-    this.face = '50137',
-    this.head = '12016',
-    this.body = '2016',
-    this.hat,
-    this.overall,
-    this.cash,
-    this.top = '1040036',
-    this.bottom = '1060026',
-    this.cape,
-    this.glove,
-    this.shoes,
-    this.shield,
-  });
+  // MyCharacter({
+  //   this.hair = '68090',
+  //   this.face = '50137',
+  //   this.head = '12016',
+  //   this.body = '2016',
+  //   this.hat,
+  //   this.overall,
+  //   this.cash,
+  //   this.top = '1040036',
+  //   this.bottom = '1060026',
+  //   this.cape,
+  //   this.glove,
+  //   this.shoes,
+  //   this.shield,
+  //   this.faceAccessory,
+  //   this.eyeDecoration,
+  //   this.earrings,
+  // });
 
   void setMyCharacter({required String subCategory, required String itemId}) {
     switch (subCategory) {
@@ -43,17 +50,24 @@ class MyCharacter {
         head = itemId;
         body = itemId.substring(1);
         break;
-      case 'Top':
-        top = itemId;
-        break;
-      case 'Bottom':
-        bottom = itemId;
+      case 'Hat':
+        hat = itemId;
         break;
       case 'Overall':
         overall = itemId;
+        top = '1040036';
+        bottom = '1060026';
         break;
       case 'Cash':
         cash = itemId;
+        break;
+      case 'Top':
+        top = itemId;
+        overall = null;
+        break;
+      case 'Bottom':
+        bottom = itemId;
+        overall = null;
         break;
       case 'Cape':
         cape = itemId;
@@ -64,8 +78,17 @@ class MyCharacter {
       case 'Shoes':
         shoes = itemId;
         break;
-      default:
+      case 'Shield':
         shield = itemId;
+        break;
+      case 'Face Accessory':
+        faceAccessory = itemId;
+        break;
+      case 'Eye Decoration':
+        eyeDecoration = itemId;
+        break;
+      default:
+        earrings = itemId;
     }
   }
 
@@ -103,6 +126,15 @@ class MyCharacter {
     if (shield != null) {
       items += addVersionAndRegion(shield!);
     }
+    if (faceAccessory != null) {
+      items += addVersionAndRegion(faceAccessory!);
+    }
+    if (eyeDecoration != null) {
+      items += addVersionAndRegion(eyeDecoration!);
+    }
+    if (earrings != null) {
+      items += addVersionAndRegion(earrings!);
+    }
 
     return items;
   }
@@ -112,5 +144,3 @@ class MyCharacter {
     return 'https://maplestory.io/api/Character/$middle/stand1/0/?renderMode=2';
   }
 }
-  // final String head = 'https://maplestory.io/api/Character/';
-  // const String tail = '/stand1/0/?renderMode=2';
