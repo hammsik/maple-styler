af4061390c4c78b5eac473d9830ffe4a5695df93
hammsik <asbbrp@gmail.com>
2023-11-15 21:40:15 +0900

    Style: 쓰지 않는 패키지 import 제거

--- lib/my_search_box.dart
+++ lib/my_search_box.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:maple_closet/my_item_list.dart';
 
 class SearchBox extends StatefulWidget {
   final List<List<List<dynamic>>> itemList;
