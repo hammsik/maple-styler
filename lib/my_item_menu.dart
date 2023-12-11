543ccdeec58e28f7dac8e0ae1348baffc8ee46cc
hammsik <asbbrp@gmail.com>
2023-12-11 15:16:22 +0900

    Fix: item menu if문 삭제

--- lib/my_item_menu.dart
+++ lib/my_item_menu.dart
@@ -22,73 +22,69 @@ class ItemMenu extends StatefulWidget {
 class _ItemMenu extends State<ItemMenu> {
   @override
   Widget build(BuildContext context) {
-    if (widget.currentTool.isNeedMenu) {
-      return MenuAnchor(
-        builder:
-            (BuildContext context, MenuController controller, Widget? child) {
-          return Material(
-            color: Colors.transparent,
-            child: InkWell(
-              splashColor: Colors.grey.withOpacity(0.4),
-              borderRadius: BorderRadius.circular(8),
-              onTap: () {
-                if (controller.isOpen) {
-                  controller.close();
-                } else {
-                  controller.open();
-                }
-              },
-              child: Ink(
-                height: 40,
-                width: 101,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(8),
-                  color: const Color.fromARGB(255, 230, 222, 218),
-                ),
-                child: Row(mainAxisAlignment: MainAxisAlignment.end, children: [
-                  Expanded(
-                    child: Text(
-                      widget.currentMenuItem,
-                      textAlign: TextAlign.center,
-                      style: GoogleFonts.nanumMyeongjo(
-                          color: const Color.fromARGB(255, 0, 0, 0),
-                          fontSize: 12,
-                          fontWeight: FontWeight.w700),
-                    ),
-                  ),
-                  const Icon(Icons.keyboard_arrow_down_rounded),
-                  const SizedBox(
-                    width: 8,
-                  )
-                ]),
-              ),
-            ),
-          );
-        },
-        menuChildren: [
-          ...widget.currentTool.menuList!.map(
-            (item) => Container(
-              margin: const EdgeInsets.all(2),
+    return MenuAnchor(
+      builder:
+          (BuildContext context, MenuController controller, Widget? child) {
+        return Material(
+          color: Colors.transparent,
+          child: InkWell(
+            splashColor: Colors.grey.withOpacity(0.4),
+            borderRadius: BorderRadius.circular(8),
+            onTap: () {
+              if (controller.isOpen) {
+                controller.close();
+              } else {
+                controller.open();
+              }
+            },
+            child: Ink(
+              height: 40,
+              width: 101,
               decoration: BoxDecoration(
-                  color: const Color.fromARGB(255, 230, 222, 218),
-                  borderRadius: BorderRadius.circular(4)),
-              child: MenuItemButton(
-                onPressed: () =>
-                    widget.buttonClicked(item[0], item[1], int.parse(item[2])),
-                child: Text(
-                  item[0],
-                  style: GoogleFonts.nanumMyeongjo(
-                      color: const Color.fromARGB(255, 0, 0, 0),
-                      fontSize: 12,
-                      fontWeight: FontWeight.w700),
+                borderRadius: BorderRadius.circular(8),
+                color: const Color.fromARGB(255, 230, 222, 218),
+              ),
+              child: Row(mainAxisAlignment: MainAxisAlignment.end, children: [
+                Expanded(
+                  child: Text(
+                    widget.currentMenuItem,
+                    textAlign: TextAlign.center,
+                    style: GoogleFonts.nanumMyeongjo(
+                        color: const Color.fromARGB(255, 0, 0, 0),
+                        fontSize: 12,
+                        fontWeight: FontWeight.w700),
+                  ),
                 ),
+                const Icon(Icons.keyboard_arrow_down_rounded),
+                const SizedBox(
+                  width: 8,
+                )
+              ]),
+            ),
+          ),
+        );
+      },
+      menuChildren: [
+        ...widget.currentTool.menuList!.map(
+          (item) => Container(
+            margin: const EdgeInsets.all(2),
+            decoration: BoxDecoration(
+                color: const Color.fromARGB(255, 230, 222, 218),
+                borderRadius: BorderRadius.circular(4)),
+            child: MenuItemButton(
+              onPressed: () =>
+                  widget.buttonClicked(item[0], item[1], int.parse(item[2])),
+              child: Text(
+                item[0],
+                style: GoogleFonts.nanumMyeongjo(
+                    color: const Color.fromARGB(255, 0, 0, 0),
+                    fontSize: 12,
+                    fontWeight: FontWeight.w700),
               ),
             ),
-          )
-        ],
-      );
-    } else {
-      return const Text('null');
-    }
+          ),
+        )
+      ],
+    );
   }
 }
